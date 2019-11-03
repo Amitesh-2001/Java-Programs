8f57fab11809f4bcfbafc13caf6b4c14cdc573ca
Amitesh-2001 <55495888+Amitesh-2001@users.noreply.github.com>
2019-11-03 23:37:56 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,9 +1,19 @@
-package labPrograms;
+package Programs.java;
 
 public class Circle {
+    double R;
+    Circle(double R){
+        this.R=R;
+    }
 
-	public Circle() {
-		// TODO Auto-generated constructor stub
-	}
+    public Circle() {
 
+    }
+
+
+    void areaOfCircle(){
+        double area;
+        area=3.14*R*R;
+        System.out.println("area of circle= "+area);
+    }
 }
