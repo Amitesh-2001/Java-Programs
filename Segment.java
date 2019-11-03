c7f1ad5b754ae51d91357d24a1257274274ce44c
Amitesh-2001 <55495888+Amitesh-2001@users.noreply.github.com>
2019-11-03 23:39:58 +0530

    Update Segment.java

--- Segment.java
+++ Segment.java
@@ -1,9 +1,15 @@
-package labPrograms;
-
-public class Segment {
-
-	public Segment() {
-		// TODO Auto-generated constructor stub
-	}
-
+package Programs.java;
+import java.lang.Math;
+public class Segment extends Circle {
+    double L;
+    Segment(double L, double R){
+        super();
+        this.L=L;
+        this.R=R;
+    }
+    void areaOfSegment(){
+        double area;
+        area=(R*R*((R-L)/R)-(R-L)*Math.pow((2*R*L-L*L),0.5));
+        System.out.println("area of segment= "+area);
+    }
 }
