231e1d563a5c95d402a58e8257f4e50a370faf98
Amitesh-2001 <55495888+Amitesh-2001@users.noreply.github.com>
2019-12-06 00:20:54 +0530

    Add files via upload

---
+++ Faculty.java
@@ -0,0 +1,9 @@
+package Programs.java;
+
+public class Faculty {
+    public String[] members_name=new String[10];
+    public String[] designation=new String[10];
+    public int[] age=new int[10];
+    public int[] years_of_experience=new int[10];
+    public int[] subjects_handled=new int[10];
+}
