b4b81d0782962c51ceb61e5d52f54d781dca380d
Amitesh-2001 <55495888+Amitesh-2001@users.noreply.github.com>
2019-12-23 02:50:18 +0530

    Update MatrixMultiplication.java

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -1,82 +1,82 @@
+package Programs.java;
+
 import java.util.*;
 
- class Matrix {
-	int i,m,n,p,q;
-	int j,k;
-	int[][] A = new int[20][20];
-	int[][] B = new int[20][20];
-	int[][] C = new int[20][20];
+class MyMatrix {
+    int row , column;
+    int[][] M;
 
-	void readMatrix(){
-		System.out.println("enter the order of the 1st matrix");
-		Scanner sc = new Scanner(System.in);
-		 m = sc.nextInt();
-		 n = sc.nextInt();
-		System.out.println("enter the elements of the 1st matrix");
-		for(i=0 ; i<m ; i++){
-			for(j = 0; j<n ; j++){
-				A[i][j] = sc.nextInt();
-			}
-		}
-		
-		System.out.println("enter the order of the 2nd matrix");
-		 p = sc.nextInt();
-		 q = sc.nextInt();
-		System.out.println("enter the elements of the 2nd matrix");
-		for(i=0 ; i<p ; i++){
-			for(j = 0; j<q ; j++){
-				B[i][j] = sc.nextInt();
-			}
-		}
-	}
-	
-	void displayMatrix(){
-		System.out.println("matrix1:");
-		for(i=0 ; i<m ; i++){
-			for(j = 0; j<n ; j++){
-				System.out.print(A[i][j]+" ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println("matrix2:");
-		for(i=0 ; i<p ; i++){
-			for(j = 0; j<q ; j++){
-				System.out.print(B[i][j]+" ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println("Matrix3:");
-		for(i=0 ; i<m ; i++){
-			for(j = 0; j<q ; j++){
-				System.out.print(C[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
-	
-	void multiplyMatrix(){
-		if(n!=p){
-			System.out.println("matrix multiplication is not possible");
-		}
-		else{
-			for(i = 0 ; i< m ; i++){
-				for(j = 0 ; j<q ; j++ ){
-					for(k = 0 ; k<n ; k++)
-						C[i][j] += A[i][k]*B[k][j]; 
-				}
-			}
-		}	
-	}
+
+    MyMatrix(int r , int c){
+        row=r;
+        column=c;
+        M = new int[row][column];
+    }
+
+
+
+
+    void readMatrix(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the elements of the  matrix");
+
+        for(int i=0 ; i<row ; i++){
+            for(int j = 0; j<column ; j++){
+                M[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+    void displayMatrix(){
+        System.out.println("matrix:");
+        for(int i=0 ; i<row ; i++){
+            for(int j = 0; j<column ; j++){
+                System.out.print(M[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    void multiplyMatrix(MyMatrix P){
+
+       MyMatrix D = new MyMatrix(this.row,P.column);
+        if(this.column != P.row){
+            System.out.println("matrix multiplication is not possible");
+        }
+        else{
+            for(int i = 0 ; i< this.row ; i++){
+                for(int j = 0 ; j<P.column ; j++ ){
+                    for(int k = 0 ; k<this.column ; k++)
+                        D.M[i][j] += this.M[i][k]*P.M[k][j];
+                }
+            }
+        }
+        D.displayMatrix();
+    }
 }
-public class MyMatrix {
+public class Matrix {
 
-	public static void main(String[] args){
-		Matrix m = new Matrix();
-		m.readMatrix();
-		m.multiplyMatrix();
-		m.displayMatrix();
-	}
+    public static void main(String[] args){
+        System.out.println("enter the order of the 1st matrix");
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println("enter the order of the 2nd matrix");
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+
+
+        MyMatrix X = new MyMatrix(m,n);
+        MyMatrix Y = new MyMatrix(p,q);
+
+        X.readMatrix();
+        Y.readMatrix();
+        X.displayMatrix();
+        Y.displayMatrix();
+        X.multiplyMatrix(Y);
+
+    }
 
 }
